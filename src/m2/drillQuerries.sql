fff7619edd8cd878b4903978cce2e969f6ad10df
agamatlab <agamatlab@sjtu.edu.cn>
2025-07-19 19:06:31 +0800

    feat(p1m2): fixed SQL Syntax [build joj]

--- src/m2/drillQuerries.sql
+++ src/m2/drillQuerries.sql
@@ -42,7 +42,7 @@ LIMIT 1;
 -- Query to find the longest song in the dataset
 SELECT
   duration,
-  artist_name,
+  artist_name
 FROM dfs.`/Users/aghamatlabakbarzade/Documents/Courses/ECE4721/p1team02/src/m1/data/aggregate.avro`
 ORDER BY
   duration DESC
